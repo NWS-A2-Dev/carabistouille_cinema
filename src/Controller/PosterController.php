a2e609296a11a928458c653e8c2867b7df6d0be6
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2022-01-14 15:03:26 +0100

    Ajout du form

--- src/Controller/PosterController.php
+++ src/Controller/PosterController.php
@@ -3,21 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Poster;
+use App\Form\PosterFormType;
 use App\Repository\PosterRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PosterController extends AbstractController
 {
     #[Route('/poster', name: 'poster')]
-    public function index(PosterRepository $repo): Response
+    public function index(PosterRepository $repo, ManagerRegistry $mr, Request $r): Response
     {
-        dd($repo->findAll());
+        $form = $this->createForm(PosterFormType::class);
+
+        $form->handleRequest($r);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $t = $form->getData();
+            $manager = $mr->getManager();
+
+            $manager->persist($t);
+            $manager->flush();
+        }
+
+        $posters = $repo->findAll();
 
         return $this->render('poster/index.html.twig', [
             'controller_name' => 'PosterController',
+            'form' => $form->createView(),
+            'posters' => $posters
         ]);
     }
 
